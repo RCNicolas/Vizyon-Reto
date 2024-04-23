ac7bcd4b08ba73594eb67a9b59beee5dd38202d5
RCNicolas <114864432+RCNicolas@users.noreply.github.com>
2024-04-23 11:51:35 -0500

    feat: :sparkles: empezando controllers

---
+++ vizyon/src/main/java/com/example/vizyon/controllers/FormaPagoController.java
@@ -0,0 +1,20 @@
+package com.example.vizyon.controllers;
+
+
+import com.example.vizyon.services.FormaPagoService;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin("*")
+@PreAuthorize("hasRole('ADMIN')")
+@SecurityRequirement(name = "bearerAuth")
+@RequestMapping("/formas-pago")
+public class FormaPagoController {
+    @Autowired
+    FormaPagoService formaPagoService;
+}
---
+++ vizyon/src/main/java/com/example/vizyon/controllers/InventarioController.java
@@ -0,0 +1,19 @@
+package com.example.vizyon.controllers;
+
+import com.example.vizyon.services.InventarioService;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin("*")
+@PreAuthorize("hasRole('ADMIN')")
+@SecurityRequirement(name = "bearerAuth")
+@RequestMapping("/inventarios")
+public class InventarioController {
+    @Autowired
+    InventarioService inventarioService;
+}
