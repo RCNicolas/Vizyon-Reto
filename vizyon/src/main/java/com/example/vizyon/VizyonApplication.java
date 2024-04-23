b9552c97cec2d2079f3e85da1c9b6a5674cf6217
RCNicolas <114864432+RCNicolas@users.noreply.github.com>
2024-04-23 06:34:48 -0500

    feat: :tada: Inicializando repositorio

---
+++ vizyon/src/main/java/com/example/vizyon/VizyonApplication.java
@@ -0,0 +1,13 @@
+package com.example.vizyon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VizyonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VizyonApplication.class, args);
+	}
+
+}
---
+++ vizyon/src/test/java/com/example/vizyon/VizyonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.vizyon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VizyonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
